d8811881e35a148bdd01ec40993bf2b7ce567d70
tcxy <du.wei@uwlax.edu>
2017-02-11 16:53:29 -0600

    Create Pilot

---
+++ src/SqaureTacer.java
@@ -0,0 +1,10 @@
+import lejos.robotics.navigation.DifferentialPilot;
+
+/**
+ * Trace Sqaure
+ * @author Wei Du
+ *
+ */
+public class SqaureTacer {
+	DifferentialPilot pilot; // The pilot represent the robot
+}
