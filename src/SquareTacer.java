0ca1d36fb576d0173680200698bdb80525cb1322
puck10 <praychan10@gmail.com>
2017-02-11 18:05:36 -0600

    Test New Method

--- src/SquareTacer.java
+++ src/SquareTacer.java
@@ -2,7 +2,7 @@ import lejos.hardware.motor.Motor;
 import lejos.robotics.navigation.DifferentialPilot;
 
 /**
- * Trace Sqaure
+ * Trace Square
  * @author Wei Du
  *
  */
@@ -12,13 +12,15 @@ public class SquareTacer {
 	public void drawSquare(float length) {
 		for (int i = 0; i < 4; i++) {
 			pilot.travel(length);
-			pilot.rotate(90);
+			pilot.rotate(180);
+			//pilot.steer(turnRate, angle, immediateReturn);
+			//pilot.rotateLeft();
 		}
 	}
 	
 	public static void main(String[] args) {
 		SquareTacer sq = new SquareTacer();
-		sq.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.C, Motor.B);
-		sq.drawSquare(10);
+		sq.pilot = new DifferentialPilot(1.95f, 3.62f, Motor.C, Motor.B);
+		sq.drawSquare(12);
 	}
 }
